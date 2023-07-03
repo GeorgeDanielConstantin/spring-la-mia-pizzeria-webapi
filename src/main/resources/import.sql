398fa2a460083d036a53fa55da7c9c15eb2b7cf6
GeorgeDanielConstantin <danielgconstantin@gmail.com>
2023-07-03 17:22:34 +0200

    Add auth

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -37,3 +37,9 @@ INSERT INTO `pizza_ingredient` (`pizza_id`, `ingredient_id`) VALUES (4, 4)
 INSERT INTO `pizza_ingredient` (`pizza_id`, `ingredient_id`) VALUES (4 ,5)
 INSERT INTO `pizza_ingredient` (`pizza_id`, `ingredient_id`) VALUES (4, 6)
 INSERT INTO `pizza_ingredient` (`pizza_id`, `ingredient_id`) VALUES (4, 7)
+INSERT INTO roles (id, name) VALUES(1,'ADMIN');
+INSERT INTO roles (id, name) VALUES(2,'USER');
+INSERT INTO users (id, email, first_name, last_name, password) VALUES(1, 'admin@email.com', 'Daniel', 'Constantin', '{noop}admin');
+INSERT INTO users (id, email, first_name, last_name, password) VALUES(2, 'jane@email.com', 'George', 'Constantin', '{noop}user');
+INSERT INTO users_roles (roles_id, user_id) VALUES(1, 1);
+INSERT INTO users_roles (roles_id, user_id) VALUES(2, 2);
